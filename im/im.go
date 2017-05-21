b79c549bd7e6bf4ddd567e589cd1dc07bd4afcfc
tttlkkkl <yehong0000@163.com>
2017-05-21 23:48:23 +0800

    忽略的文件

--- im/im.go
+++ im/im.go
@@ -1,42 +1,45 @@
 package im
 
 import (
-	"golang.org/x/net/websocket"
 	"log"
 	"net/http"
 	"time"
+
+	"golang.org/x/net/websocket"
 )
 
 // Client 客户端连接标识
 type Client struct {
-	Id          *websocket.Conn
+	ID          *websocket.Conn
 	ConnectTime int64
 }
 
 //ConnectPool 客户端连接池
 var ConnectPool = make([]*Client, 0)
 
-func ImServer() {
-	//go eventPool()
+//WebsocketServer websocket服务器
+func WebsocketServer() {
+	go eventPool()
+	log.Println("xxxx")
 	http.Handle("/", websocket.Handler(Handle))
+	log.Println("im websocket 服务启动...")
 	if err := http.ListenAndServe(":3001", nil); err != nil {
 		log.Fatalln("websocket端口监听错误:", err)
-	} else {
-		log.Println("im websocket 服务启动...")
 	}
 }
 
 //Handle 处理
 func Handle(ws *websocket.Conn) {
-	client := Client{Id: ws, ConnectTime: time.Now().Unix()}
+	client := Client{ID: ws, ConnectTime: time.Now().Unix()}
 	ConnectPool = append(ConnectPool, &client)
 }
 func eventPool() {
 	//var err error
 	for {
-		for client := range ConnectPool {
+		for k, client := range ConnectPool {
 			//var reply string
-			println("ws:", client)
+			println("ws_v:", client)
+			println("ws_k", k)
 			// if err = websocket.Message.Receive(client.Id, &reply); err != nil {
 			// 	log.Println("接收消息失败!")
 			// 	break
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	//启动http服务
 	go web.StaticHTTPServer()
 	//启动websocket服务
-	go im.ImServer()
+	go im.WebsocketServer()
 	//阻止主进程退出
 	select {}
 }

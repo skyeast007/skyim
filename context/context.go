dfce34440a4823036fdb9abba6e61df98ed0e0c1
tttlkkkl <yehong0000@163.com>
2017-05-25 21:44:12 +0800

    删除无用文件夹，添加版本信息

--- context/context.go
+++ context/context.go
@@ -5,6 +5,8 @@ type Context struct {
 	Options *Options
 	Tool    *Tool
 	Log     *Log
+	//Version 版本信息
+	Version string
 }
 
 //NewCtx 获取ctx
@@ -13,5 +15,6 @@ func NewCtx() *Context {
 	c.Options = NewOption()
 	c.Tool = new(Tool)
 	c.Log = new(Log)
+	c.Version = "1.0"
 	return c
 }
